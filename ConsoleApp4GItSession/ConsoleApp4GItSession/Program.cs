d30daf0d9ba68f09dba135f56df14f550169263f
ahmed <ahmed_h261@yahoo.com>
2025-04-12 16:13:28 +0200

    New Hello Message

--- ConsoleApp4GItSession/ConsoleApp4GItSession/Program.cs
+++ ConsoleApp4GItSession/ConsoleApp4GItSession/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcom, to programming World!");
+
+Console.WriteLine(NewWelcomeMessage());
+
+
+
+string NewWelcomeMessage()
+{
+    return "Hello Message";
+}
